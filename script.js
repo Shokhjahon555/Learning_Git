151f5f5aef35177d9280915aabd780821f26dd7d
Shokhjahon555 <136553626+Shokhjahon555@users.noreply.github.com>
2024-05-19 14:07:13 +0000

    Added new object

---
+++ script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+let thief = {
+    jacket : "black",
+    height : 1.7,
+    colors : {
+        hair: "yellow",
+        skin: "brown",
+    }
+};
+
+console.log(Object.keys(thief));
+console.log("how are you?")
